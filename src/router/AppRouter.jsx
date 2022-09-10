060e9d30a014f4c8a67704bde58bf1f68d8028a9
MatiasUrielGluck <matiasugluck@gmail.com>
2022-09-10 11:35:20 -0300

    creating index.js files to export

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
 
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from "../auth";
 import { Navbar } from "../ui";
 
 export const AppRouter = () => {
