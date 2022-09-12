c7e12556bde52375e5b7db5865942ad3822b8d7d
MatiasUrielGluck <matiasugluck@gmail.com>
2022-09-12 11:03:24 -0300

    Creating search feature

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,38 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
-  return <h1>Search page</h1>;
-}; 
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+
+          <div className="alert alert-danger">
+            Hero <b>ABC</b> not found
+          </div>
+
+          <HeroCard />
+        </div>
+      </div>
+    </>
+  );
+};
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -34,6 +34,15 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
