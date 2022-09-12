855567572a31ea875ec070e947f9f51b4bc5030f
MatiasUrielGluck <matiasugluck@gmail.com>
2022-09-12 14:48:29 -0300

    Finishing development phase 1

---
+++ src/heroes/helpers/getHeroesByName.js
@@ -0,0 +1,10 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByName = (name = "") => {
+  const rawName = name.toLowerCase().trim();
+  if (name.length === 0) return [];
+
+  return heroes.filter((hero) =>
+    hero.superhero.toLowerCase().includes(rawName)
+  );
+};
